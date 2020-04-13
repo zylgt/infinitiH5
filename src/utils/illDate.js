020da2995455818edba7acdb8d9424ca62fce106
wangshenzhen <wangshenzhen@ihealthlabs.com.cn>
2020-04-13 13:45:58 +0800

    增加量表

--- src/utils/illDate.js
+++ src/utils/illDate.js
@@ -53,27 +53,70 @@ export const illDate = {
             isShowInput:false,
             title:'请输入医院医生诊断的疾病名称',
             inputValue:'',
+        }
+    ],
+    ill2:[
+        {
+            key:1,
+            question:{
+                first:'',
+                second:''
+            },  // 问题
+            answer:{
+                firstAnswer:[            //答案
+                    {
+                        title:'一周内',    //答案名称
+                        showInput:false,   //是否展示答案下方输入框
+                        name:'week'
+                    },
+                    {
+                        title:'一个月内',
+                        showInput:false,
+                        name:'month'
+                    },
+                    {
+                        title:'半年内',
+                        showInput:false,
+                        name:'half_year'
+                    },
+                    {
+                        title:'一年内',
+                        showInput:false,
+                        name:'year'
+                    }
+                ],
+                secondAnswer:[
+                    {
+                        title:'一周内',    //答案名称
+                        showInput:false,   //是否展示答案下方输入框
+                        name:'week'
+                    },
+                    {
+                        title:'一个月内',
+                        showInput:false,
+                        name:'month'
+                    },
+                    {
+                        title:'半年内',
+                        showInput:false,
+                        name:'half_year'
+                    },
+                    {
+                        title:'一年内',
+                        showInput:false,
+                        name:'year'
+                    }
+                ]
+            },
+            chooseAnswer:{
+
+            },    //选择的答案
+            answerMore:false, // 是否支持多选
+            isMore:false, // 是否有第二个问题
+            isShowMore:false, // 是否展示第二个问题
+            title:'',  // 输入框的问题
+            inputValue:'', //输入内容
+            inputMore:false //是否支持多选
         },
-        // {
-        //     key:'three',
-        //     question:'是否吃过药？',
-        //     answer:[
-        //         {
-        //             title:'没有',
-        //             showInput:false
-        // name:'no'
-        //         },
-        //         {
-        //             title:'有',
-        //             showInput:true,
-        // name:'yes'
-        //         },
-        //     ],
-        //     choose:'',
-        //     isInput:true,
-        //     isShowInput:false,
-        //     title:'请输入吃的药的名称',
-        //     inputValue:'',
-        // }
     ]
 }
